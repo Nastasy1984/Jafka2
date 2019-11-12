1c798b567bcd64ffdb910ac457e66493d2ad2926
Anastasia Spiridonova <asb_gzas@mail.ru>
2019-11-12 16:48:45 +0300

    Adding timestamp to JSON String added

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -20,21 +20,31 @@ import org.springframework.messaging.MessageChannel;
 public class AppConfig {
 	@Bean
 	public IntegrationFlow fromKafkaFlow(ConsumerFactory<String, String> consumerFactory) {
-		return IntegrationFlows
-				.from(Kafka.messageDrivenChannelAdapter(consumerFactory, "t"))
-				//.<String, String>transform(String::toUpperCase)
-				//.<String, String>transform((p) -> p + "YYY")
-				//.<String>handle((p, h) -> p + "VAS")
-				.<String>handle((p, h) -> p + Timestamp.from(Instant.now()))
-				//sending output to the direct channel myChannel
-				.channel("myChannel")
-				.get();
+		return IntegrationFlows.from(Kafka.messageDrivenChannelAdapter(consumerFactory, "a"))
+				// .<String, String>transform(String::toUpperCase)
+				// .<String, String>transform((p) -> p + "YYY")
+				// .<String>handle((p, h) -> p + "VAS")
+				//.<String>handle((p, h) -> p + Timestamp.from(Instant.now()))
+				//deleting the last "}" and adding field with timestamp
+				.<String>handle((p, h) -> {
+					String toAdd = ", \"timestamp\": " + Timestamp.from(Instant.now()) + "}";
+					return p.substring(0, p.length() - 1) + toAdd;
+				})
+
+				// sending output to the direct channel myChannel
+				.channel("myChannel").get();
 	}
 	
 	//creating new topic for outbound flow
 	@Bean
 	public NewTopic topic() {
-		return new NewTopic("t-t", 1, (short) 1);
+		return new NewTopic("a-a", 1, (short) 1);
+	}
+	
+	//creating new topic for inbound flow
+	@Bean
+	public NewTopic initialTopic() {
+		return new NewTopic("a", 1, (short) 1);
 	}
 	
 	// channel from integration flow
@@ -47,7 +57,7 @@ public class AppConfig {
 	@Bean
 	public IntegrationFlow outFlow(KafkaTemplate<String, String> kafkaTemplate) {
 		return IntegrationFlows.from("myChannel")
-				.handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic("t-t")).
+				.handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic("a-a")).
 				get();
 	}
 
