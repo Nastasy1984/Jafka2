86ab5a131a8617c8fcd46448c89b56e89b423265
Anastasia Spiridonova <asb_gzas@mail.ru>
2019-11-12 17:40:33 +0300

    Properties added

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.time.Instant;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,9 +19,16 @@ import org.springframework.messaging.MessageChannel;
 @Configuration
 @ConfigurationProperties("kafka")
 public class AppConfig {
+	
+	 @Value("${kafka.topic.initial-topic}")
+	  private String initialKafkaTopic;
+	 
+	 @Value("${kafka.topic.new-topic}")
+	  private String newKafkaTopic;
+	
 	@Bean
 	public IntegrationFlow fromKafkaFlow(ConsumerFactory<String, String> consumerFactory) {
-		return IntegrationFlows.from(Kafka.messageDrivenChannelAdapter(consumerFactory, "a"))
+		return IntegrationFlows.from(Kafka.messageDrivenChannelAdapter(consumerFactory, initialKafkaTopic))
 				// .<String, String>transform(String::toUpperCase)
 				// .<String, String>transform((p) -> p + "YYY")
 				// .<String>handle((p, h) -> p + "VAS")
@@ -38,13 +46,13 @@ public class AppConfig {
 	//creating new topic for outbound flow
 	@Bean
 	public NewTopic topic() {
-		return new NewTopic("a-a", 1, (short) 1);
+		return new NewTopic(newKafkaTopic, 1, (short) 1);
 	}
 	
 	//creating new topic for inbound flow
 	@Bean
 	public NewTopic initialTopic() {
-		return new NewTopic("a", 1, (short) 1);
+		return new NewTopic(initialKafkaTopic, 1, (short) 1);
 	}
 	
 	// channel from integration flow
@@ -57,7 +65,7 @@ public class AppConfig {
 	@Bean
 	public IntegrationFlow outFlow(KafkaTemplate<String, String> kafkaTemplate) {
 		return IntegrationFlows.from("myChannel")
-				.handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic("a-a")).
+				.handle(Kafka.outboundChannelAdapter(kafkaTemplate).topic(newKafkaTopic)).
 				get();
 	}
 
